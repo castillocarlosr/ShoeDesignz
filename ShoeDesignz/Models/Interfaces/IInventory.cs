991ce81c4a1f2d27f0598e2dd47d840aa1dc7c45
castillocarlosr <castillocarlosr@gmail.com>
2019-02-20 11:31:47 -0800

    Added Inventory Model and Inventory Interface

---
+++ ShoeDesignz/Models/Interfaces/IInventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoeDesignz.Models.Interfaces
+{
+    interface IInventory
+    {
+        //Create
+        Task CreateInventory(Inventory inventory);
+
+        //Read
+        Task<Inventory> GetInventory(int id);
+
+        Task<IEnumerable<Inventory>> GetInventories();
+
+        //Edit
+        Task EditInventory(Inventory inventory);
+
+        //Delete
+        Task DeleteInventory(int id);
+    }
+}
