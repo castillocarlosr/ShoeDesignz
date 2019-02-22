364bad1c172a464d0d280411d61140ffe06119db
castillocarlosr <castillocarlosr@gmail.com>
2019-02-22 12:13:44 -0800

    Added edu authorization email

---
+++ ShoeDesignz/Controllers/PolicyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoeDesignz.Controllers
+{
+    [Authorize(Policy = "EduEmail")]
+    public class PolicyController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Education()
+        {
+            return View();
+        }
+    }
+}
