b46437d050fabe587a7d33302e86ab71bf31941c
castillocarlosr <castillocarlosr@gmail.com>
2019-03-04 14:16:02 -0800

    Pushing to master with what we have.

--- ShoeDesignz/Controllers/OrderController.cs
+++ ShoeDesignz/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using ShoeDesignz.Models;
 using ShoeDesignz.Models.Interfaces;
 using System.Threading.Tasks;
@@ -8,21 +9,45 @@ namespace ShoeDesignz.Controllers
     public class OrderController : Controller
     {
         private readonly IOrder _context;
+        private IConfiguration _configuration;
 
         public OrderController(IOrder context)
         {
             _context = context;
         }
+        
+        //public async Task <IActionResult> Details()
+        //{
 
-        public async Task <IActionResult> Details()
-        {
-
-        }
+        //}
         public async Task <IActionResult> Index()
         {
             var email = User.Identity.Name;
             Order order = await _context.Getorder(email);
             return View(order);
         }
+
+
+
+        /*
+        [HttpPost]
+        public IActionResult CompleteOrder(CreditCard card)
+        {
+            Payment payment = new Payment(_configuration);
+            string cardSent = payment.Run();
+
+            if(cardSent == "OK")
+            {
+                //Send to a page that says payment send sucessfull.  Click here to retrurn to store.
+                return View();
+            }
+            else
+            {
+                //Send to page that says, "Oh no, something went wrong with your payment.
+                //Try another credit card
+                return View();
+            }
+        }*/
+        
     }
 }
