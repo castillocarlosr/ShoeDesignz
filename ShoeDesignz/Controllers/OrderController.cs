123d58e11a4a107512a63128f96ab6a12c446f75
castillocarlosr <castillocarlosr@gmail.com>
2019-03-04 19:50:36 -0800

    stash

--- ShoeDesignz/Controllers/OrderController.cs
+++ ShoeDesignz/Controllers/OrderController.cs
@@ -24,29 +24,6 @@ namespace ShoeDesignz.Controllers
             var email = User.Identity.Name;
             Order order = await _context.Getorder(email);
             return View(order);
-        }
-
-
-
-        /*
-        [HttpPost]
-        public IActionResult CompleteOrder(CreditCard card)
-        {
-            Payment payment = new Payment(_configuration);
-            string cardSent = payment.Run();
-
-            if(cardSent == "OK")
-            {
-                //Send to a page that says payment send sucessfull.  Click here to retrurn to store.
-                return View();
-            }
-            else
-            {
-                //Send to page that says, "Oh no, something went wrong with your payment.
-                //Try another credit card
-                return View();
-            }
-        }*/
-        
+        }        
     }
 }
