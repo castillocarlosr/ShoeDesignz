da3ef8169ece1265847d745e65cb01208c472f0c
castillocarlosr <castillocarlosr@gmail.com>
2019-03-05 10:11:26 -0800

    pushed credit card changes

--- ShoeDesignz/Controllers/OrderController.cs
+++ ShoeDesignz/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using ShoeDesignz.Models;
 using ShoeDesignz.Models.Interfaces;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ShoeDesignz.Controllers
@@ -15,15 +16,48 @@ namespace ShoeDesignz.Controllers
             _context = context;
         }
         
+
         //public async Task <IActionResult> Details()
         //{
 
         //}
+
+        [HttpGet]
+        [Route("order/details/{id}")]
+        public async Task <IActionResult> Details(int id)
+        {
+            var email = User.Identity.Name;
+            Order order = await _context.GetOrder(id);
+            return View("Details",order);
+        }
+
         public async Task <IActionResult> Index()
         {
             var email = User.Identity.Name;
             Order order = await _context.Getorder(email);
             return View(order);
-        }        
+        }
+
+
+        /*
+        [HttpPost]
+        public IActionResult CompleteOrder(CreditCard card)
+        {
+            Payment payment = new Payment(_configuration);
+            string cardSent = payment.Run();
+
+            if(cardSent == "OK")
+            {
+                //Send to a page that says payment send sucessfull.  Click here to retrurn to store.
+                return View();
+            }
+            else
+            {
+                //Send to page that says, "Oh no, something went wrong with your payment.
+                //Try another credit card
+                return View();
+            }
+        }*/
+        
     }
 }
