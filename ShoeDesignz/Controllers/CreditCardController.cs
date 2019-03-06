779f6a420d09a32561899c4eac95307e23ab9f5f
castillocarlosr <castillocarlosr@gmail.com>
2019-03-06 11:19:43 -0800

    CreditCard and Experation Fully Work with User.

--- ShoeDesignz/Controllers/CreditCardController.cs
+++ ShoeDesignz/Controllers/CreditCardController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using ShoeDesignz.Models;
 
 namespace ShoeDesignz.Controllers
 {
     public class CreditCardController : Controller
     {
+        private readonly IConfiguration _configuration;
+        public CreditCardController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +20,23 @@ namespace ShoeDesignz.Controllers
         public IActionResult VerifyOrder()
         {
             return RedirectToAction("Index", "Cart");
+        }
+
+        [HttpPost]
+        public IActionResult Run(Payment paymentInside)
+        {
+            Payment payment = new Payment(_configuration);
+            string answer = payment.Run((long)paymentInside.CreditList, (int)paymentInside.ExpirationCard);
+
+            if(answer == "OK")
+            {
+                return View("PaymentComplete");
+                //return RedirectToAction("PaymentComplete", "CreditCard");
+            }
+            else
+            {
+                return RedirectToAction("PaymentError", "CreditCard");
+            }
         }
     }
 }
